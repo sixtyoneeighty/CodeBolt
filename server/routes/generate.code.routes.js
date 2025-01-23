2a5c4797d88ca9a2d7a1c8e358d4f72fe21040d0
shiva-manu <shivamani6964@gmail.com>
2025-01-23 21:01:31 +0530

    prompt enhancer route add

--- server/routes/generate.code.routes.js
+++ server/routes/generate.code.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { backendCodeGenerator,frontendCodeGenerator } from "../controllers/generate.code.controller.js";
+import { backendCodeGenerator,frontendCodeGenerator,promptEnchancer } from "../controllers/generate.code.controller.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -7,5 +7,5 @@ const router=express.Router();
 
 router.post("/generate-backend-code",backendCodeGenerator);
 router.post("/generate-frontend-code",frontendCodeGenerator);
-
+router.post("/generate-prompt",promptEnchancer)
 export default router;
